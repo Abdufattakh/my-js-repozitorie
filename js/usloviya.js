c80000714ae704e28e496870796cd6fd08d57799
Abdulfattoh <fattohf3@gmail.com>
2021-08-03 15:21:38 +0500

    commit usloviya.js

--- js/usloviya.js
+++ js/usloviya.js
@@ -1,18 +1,35 @@
 "use strict";
 
-if (4 == 5) {
-  console.log("OK");
-}else{
-console.log('Error');
+// if (4 == 5) {
+//   console.log("OK");
+// }else{
+// console.log('Error');
+// }
+
+// const a = +prompt("Сколько будет: 5 + 9 ?","");
+
+// if(a == 14){
+//     alert(true);
+// }else{
+//     alert('error');
+// }
+//
+
+// (num === 50) ? console.log('ok') : console.log('error');
+
+const num = 50;
+
+switch (num) {
+  case "49":
+    console.log("error");
+    break;
+  case "100":
+    console.log("error");
+    break;
+  case "50":
+    console.log("true");
+    break;
+  default:
+    console.log("NO working");
+    break;
 }
-
-const a = +prompt("Сколько будет: 5 + 9 ?","");
-
-if(a == 14){
-    alert(true);
-}else{
-    alert('error');
-}
-
-
-
