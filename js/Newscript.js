20113e6f87ff5752025f1964e021d8f0febfccbe
Abdulfattoh <fattohf3@gmail.com>
2021-08-03 12:56:41 +0500

    first commit

---
+++ js/Newscript.js
@@ -0,0 +1,64 @@
+"use strict";
+//====================Простое обращение с пользователям
+
+// alert('Hello world')
+
+/**confirm =>Функция confirm отображает модальное
+ * окно с текстом вопроса question и двумя кнопками:
+ * OK и Отмена.Результат – true, если нажата кнопка OK.
+ *  В других случаях – false.
+ */
+
+// const result = confirm("are you here");
+// console.log(result);
+
+// const answer = +prompt("вам есть 18?", "18");
+// console.log(answer + 5);
+// строки
+
+// const answers = [];
+// answers[0] = prompt("Как ваше имя ?", "");
+// answers[1] = prompt("Как ваше фамилия ?", "");
+// answers[2] = prompt("Сколько вам лет ?", "");
+// console.log(typeof answers);
+// console.log(null);
+
+// ========================================================================================
+
+//================== Интерполяция
+
+// const category = "toys";
+// console.log(`https://someurl.com/${category}/5`);
+
+// const user = "Ivan";
+// alert(`Првет, ${user}`);
+
+// =========================================================================================
+// =====================  Операторы
+
+// console.log('arr' + '- object');
+// console.log('arr' + '- object');
+//
+
+// ==========================  Задание 1
+const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?");
+
+// ==========================  Задание 2
+const personalMovieDB = {
+  count: numberOfFilms,
+  movies: {},
+  actors: {},
+  genres: [],
+  privat: false,
+};
+
+// ==========================  Задание 3
+const movies = {};
+const question1 = prompt("один из последных просмотренных фильмов ?", ""),
+  a = prompt("один из последных просмотренных фильмов ?", "");
+const qustion2 = +prompt("На сколько оцените его"),
+  b = +prompt("На сколько оцените его");
+
+personalMovieDB.movies[question1] = a;
+personalMovieDB.movies[qustion2] = b;
+console.log(personalMovieDB);
---
+++ js/script.js
@@ -0,0 +1,76 @@
+"use strict"; //строгий режим
+// ====================  Lesson1
+
+// Variables
+
+// let number = 5;
+// const leftBorderWidth = 1;
+
+// =============================================================================================================
+// ====================  Lesson 2
+
+// console.log(4 + "5");
+
+// let incr = 10,
+//   decr = 10;
+
+// // incr++;  // префиксные
+// // decr--;
+
+// incr++; //посфиксные
+// decr--;
+// console.log(5 % 2);
+
+// console.log(2 * 4 === 8);
+
+// // && // оператор и
+// // || оператор или
+
+// const isCheked = true,
+//       isClose = false;
+
+// console.log(isCheked || isClose);
+// =============================================================================================================
+// ====================  Lesson 3
+// Числа
+let number = 5.5;
+
+console.log(4 / 0);
+console.log("string" * 9);
+
+//Строка
+
+const person = "Alex";
+
+//Булиновая значение
+
+const bool = false;
+
+//Null => чтота не существувфюший
+// console.log(something);
+
+//Undefined
+let end;
+console.log(end);
+
+/**
+ * Object => это коллекция данных,
+ * структура которое храниться любое структура данных
+ */
+
+const obj = {
+  name: "Abdulfattoh",
+  age: "16",
+  isMarried: false,
+};
+// console.log(obj.name);
+console.log(obj["age"]);
+
+//Массив
+
+let arr = ["plumb.png", "ornage.jpg", 6, "appple.bmp",[]];
+console.log(arr[1]);
+
+
+
+
