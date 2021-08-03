4cdbd336d3d4f23e7d39b86bf45831b4739c6b87
Abdulfattoh <fattohf3@gmail.com>
2021-08-03 13:14:56 +0500

    usloviya js

---
+++ js/usloviya.js
@@ -0,0 +1,18 @@
+"use strict";
+
+if (4 == 5) {
+  console.log("OK");
+}else{
+console.log('Error');
+}
+
+const a = prompt("Сколько будет: 5 + 9 ?","");
+
+if(a == 14){
+    alert(true);
+}else{
+    alert('error');
+}
+
+
+
