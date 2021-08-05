83bc48c08c0c4c356a159a8c0f12e769db2cec75
Abdulfattoh <fattohf3@gmail.com>
2021-08-05 12:24:04 +0500

    js

---
+++ js/methods.js
@@ -0,0 +1,41 @@
+"use strict";
+
+const test = "HEllo";
+const arr = [1, 2, 3];
+// console.log(test[3]);
+console.log(test.toUpperCase());
+console.log(test.toLowerCase());
+
+let fruit = "Some fruit";
+console.log(fruit.indexOf("q"));
+
+
+// var indices = [];
+// var array = ['a', 'b', 'a', 'c', 'a', 'd'];
+// var element = 'a';
+// var idx = array.indexOf(element);
+// while (idx != -1) {
+//   indices.push(idx);
+//   idx = array.indexOf(element, idx + 1);
+// }
+
+// console.log(indices);
+
+const logg = "hello world !";
+console.log(logg.slice(6, 10));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
